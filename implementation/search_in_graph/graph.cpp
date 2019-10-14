3a01ea23c6df89bb60cfa767f05a8c14e3a89852
harshraj22 <harshraj22aug@gmail.com>
2019-10-14 21:55:14 +0530

    updated graph basic structure in cpp

--- implementation/search_in_graph/graph.cpp
+++ implementation/search_in_graph/graph.cpp
@@ -2,22 +2,34 @@
 using namespace std;
 
 class graph{
-    protected:
-        vector<vector<int> > gr;
-        vector<int> visited;
-    public:
-        graph(int vertices){
-            gr.resize(vertices,vector<int>());
-            visited.resize(vertices,-1);
-        }
+	protected:
+		// graph represented as adjacency list
+		vector<vector<int> > gr;
+		// supports building reverse graph as well
+		vector<vector<int> > rg;
+	public:
+		graph(int vertices)
+			:gr(vertices+1), rg(vertices+1){
+				// do nothing, used list initialisation
+		}
 
-        void add_edge(int par,int child,bool directed=false){
-            gr[par].push_back(child);
-            if(!directed)
-                gr[child].push_back(par);
-        }
+		// add edge between vertices (u,v)
+		void add_edge(int u, int v, bool undirected=false, bool reverse=false){
+			gr[u].push_back(v);
+			// if graph is undirected, use bidirectional edge
+			if(undirected)
+				gr[v].push_back(u);
+
+			// if we also need to build the reverse graph
+			if(reverse){
+				rg[v].push_back(U);
+				if(undirected)
+					rg[u].push_back(v);
+			}
+		}
 };
 
+// public inheritence, thus protected data instead of private
 class modified_graph: public graph{
     private:
     public:
